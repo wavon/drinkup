4918a9ad3928609048cb6b67d20c1d9c960174ba
lindseyvoskowsky <lindseyvoskowsky@gmail.com>
2014-11-03 14:17:44 -0800

    changed js

---
+++ js/app.js
@@ -0,0 +1,26 @@
+var main = function() {
+  /* Push the body and the icon nav to the right by 285px over 200ms */
+  $('.icon-menu').click(function() {
+    $('.menu').animate({
+      left: "0px"
+    }, 200);
+
+    $('body').animate({
+      left: "285px"
+    }, 200);
+  });
+
+  /* Then push them back on clicking the X */
+  $('.icon-close').click(function() {
+    $('.menu').animate({
+      left: "-285px"
+    }, 200);
+
+    $('body').animate({
+      left: "0px"
+    }, 200);
+  });
+};
+
+
+$(document).ready(main);
