5cc2e7d98b5e2b62d9c607263f1282515b3d6d5d
Ondine Gallatin <ogallatin@gmail.com>
2014-11-04 11:03:52 -0800

    Sliding menu working

--- js/app.js
+++ js/app.js
@@ -1,34 +1,27 @@
+var mainNav = function() {
+  /* Push the body and the icon nav to the right by 285px over 200ms */
+  $('.icon-menu').click(function() {
+    $('.main-nav').animate({
+      left: "0px"
+    }, 200);
 
-
-$(document).ready(function(){
-
-  var mainNav = function() {
-    $('.icon-menu').click(function() {
-      $('.main-nav').animate({
-        left: "0px"
-      }, 200);
-
-      $('.main-container').animate({
-        left: "13em"
-      }, 200);
-    });
+    $('.main-container').animate({
+      left: "285px"
+    }, 200);
+  });
 
   /* Then push them back on clicking the X */
-    $('.icon-close').click(function() {
-      $('.menu').animate({
-        left: "-285px"
-      }, 200);
+  $('.main-nav').click(function() {
+    $('.main-nav').animate({
+      left: "-285px"
+    }, 200);
 
-      $('body').animate({
-        left: "0px"
-      }, 200);
-    });
-  };
-
-$('button').click(function() {
- alert("yay!");
-});
+    $('.main-container').animate({
+      left: "0px"
+    }, 200);
+  });
+};
 
 
+$(document).ready(mainNav);
 
-});
