71ba1265bd6ae0cea118314184eaeb5ac2052e39
Brigitta Glanz <glanzb@icloud.com>
2014-11-04 13:08:29 -0800

    delete old js comment

--- js/app.js
+++ js/app.js
@@ -28,34 +28,3 @@ $(document).ready(mainNav);
 
 
 
-
-
-// //Create a list item and append to #menu
-// var $iconMenu = $("<select></select>");
-// $("#icon-menu").append($iconMenu);
-
-// //Cycle over menu links
-// $("nav a").each(function(){
-//   var $anchor = $(this);
-//   //Create an option
-//   var $option = $("<option></option>");
-
-
-//   // //Deal with selected options depending on current page
-//   // if($anchor.parent().hasClass("selected")) {
-//   //   $option.prop("selected", true);
-//   // }
-  
-//   //option's value is the href
-//   $option.val($anchor.attr("href"));
-//   //option's text is the text of link
-//   $option.text($anchor.text());
-//   //append option to select
-//   $select.append($option);
-// });
-
-// //Bind change listener to the select
-// $select.change(function(){
-//   //Go to select's location
-//   window.location = $select.val();
-// });
